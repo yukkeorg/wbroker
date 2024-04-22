ef25f43987df317693a7902e8a1f9243388c9e07
Ohshima Yusuke <ohshima.yusuke@yukke.org>
2024-04-22 23:38:12 +0900

    Update

--- app.py
+++ app.py
@@ -7,25 +7,26 @@ from bme280 import bme280_i2c
 import os
 import threading
 import time
-import pprint
 
 I2C_ADDR = 0x76
 I2C_BUS = 1
 
-influx_url = "http://yukke-server.local:8086"
+influx_url = os.environ.get("INFLUXDB_URL")
 influx_token = os.environ.get("INFLUXDB_TOKEN")
-influx_org = "yukkeorg"
-influx_bucket = "yukkeorg"
+influx_org = os.environ.get("INFLUXDB_ORG")
+influx_bucket = os.environ.get("INFLUXDB_BUCKET")
 
 lock = threading.Lock()
+
 
 class InfluxWriter:
     def __init__(self, url=None, token=None, org=None, bucket=None):
         url = url if url is not None else influx_url
         token = token if token is not None else influx_token
-        org = org if org is not None else influx_org 
-        self.client = InfluxDBClient(url=url, token=token, org=org)
-        self.write_api = self.client.write_api(wtite_options=SYNCHRONOUS)
+        org = org if org is not None else influx_org
+        client = InfluxDBClient(url=url, token=token, org=org)
+
+        self.write_api = client.write_api(wtite_options=SYNCHRONOUS)
         self.bucket = bucket if bucket is not None else influx_bucket
 
     def write(self, point, values):
@@ -35,7 +36,7 @@ class InfluxWriter:
         self.write_api.write(bucket=self.bucket, record=point)
 
 
-class Mesurement:
+class Bme280Sensor:
     def __init__(self):
         self.data = None
 
@@ -48,7 +49,7 @@ class Mesurement:
             self.data = bme280.read_all()
 
     def get_tupple(self):
-        return (self.data.temperature, 
+        return (self.data.temperature,
                 self.data.humidity,
                 self.data.pressure)
 
@@ -58,14 +59,14 @@ class MesurementThread(threading.Thread):
         threading.Thread.__init__(self)
         self.daemon = True
 
-        self.mesurement = Mesurement()
+        self.sensor = Bme280Sensor()
         self.writer = InfluxWriter()
 
     def run(self):
         tags = ["temperature", "humidity", "pressure"]
         while True:
-            self.mesurement.mesurement()
-            values = dict(zip(tags, self.mesurement.get_tupple()))
+            self.sensor.mesurement()
+            values = dict(zip(tags, self.sensor.get_tupple()))
             self.writer.write("measurement", values)
             time.sleep(10)
 
