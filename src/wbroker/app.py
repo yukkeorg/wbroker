4df200771bb679798e32d2a77f654a14967bf741
Ohshima Yusuke <ohshima.yusuke@yukke.org>
2024-09-08 22:58:52 +0900

    Add typing

--- src/wbroker/app.py
+++ src/wbroker/app.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 import os
 import threading
 import time
@@ -19,83 +21,88 @@ I2C_BUS = 1
 
 logging.basicConfig(level=logging.INFO)
 
-influx_url = os.environ.get("INFLUXDB_URL")
-influx_token = os.environ.get("INFLUXDB_TOKEN")
-influx_org = os.environ.get("INFLUXDB_ORG")
-influx_bucket = os.environ.get("INFLUXDB_BUCKET")
+env_influx_url = os.environ.get("INFLUXDB_URL")
+env_influx_token = os.environ.get("INFLUXDB_TOKEN")
+env_influx_org = os.environ.get("INFLUXDB_ORG")
+env_influx_bucket = os.environ.get("INFLUXDB_BUCKET")
 
 lock = threading.Lock()
 
 
 class InfluxWriter:
-    def __init__(self, url=None, token=None, org=None, bucket=None):
-        url = url if url is not None else influx_url
-        token = token if token is not None else influx_token
-        org = org if org is not None else influx_org
+    def __init__(
+        self,
+        url: str | None = None,
+        token: str | None = None,
+        org: str | None = None,
+        bucket: str | None = None,
+    ):
+        url = url if url is not None else env_influx_url
+        token = token if token is not None else env_influx_token
+        org = org if org is not None else env_influx_org
 
         client = InfluxDBClient(url=url, token=token, org=org)
 
         self.write_api = client.write_api(wtite_options=SYNCHRONOUS)
-        self.bucket = bucket if bucket is not None else influx_bucket
+        self.bucket = bucket if bucket is not None else env_influx_bucket
 
-    def write(self, point, values):
-        point = Point(point)
+    def write(self, point: str, values: dict[str, Any]) -> None:
+        p = Point(point)
         for k, v in values.items():
-            point.field(k, v)
-        self.write_api.write(bucket=self.bucket, record=point)
+            p.field(k, v)
+        self.write_api.write(bucket=self.bucket, record=p)
 
 
 class SO1602ADisplay:
-    def __init__(self, bus=I2C_BUS, addr=SO1602A_ADDR):
+    def __init__(self, bus: int = I2C_BUS, addr: int = SO1602A_ADDR):
         self.i2c = smbus.SMBus(bus)
         self.addr = addr
 
-    def __send_command(self, data: bytes):
+    def __send_command(self, data: int) -> None:
         with lock:
             self.i2c.write_byte_data(self.addr, 0x00, data)
 
-    def __send_data(self, data: bytes):
+    def __send_data(self, data: bytes) -> None:
         with lock:
             for i in data:
                 self.i2c.write_byte_data(self.addr, 0x40, i)
 
-    def setup(self):
+    def setup(self) -> None:
         self.all_clear()
         self.__send_command(0x0C)
 
-    def put(self, data: str):
+    def put(self, data: str) -> None:
         self.__send_data(data.encode())
 
-    def all_clear(self):
+    def all_clear(self) -> None:
         self.__send_command(0x01)
 
-    def return_home(self):
+    def return_home(self) -> None:
         self.__send_command(0x02)
 
-    def return_home_fast(self):
+    def return_home_fast(self) -> None:
         self.__send_command(0x80)
 
     return_first_line = return_home_fast
 
-    def return_second_line(self):
+    def return_second_line(self) -> None:
         self.__send_command(0xA0)
 
 
 class Bme280Sensor:
     def __init__(self):
         self.data = None
-
         bme280_i2c.set_default_i2c_address(BME280_ADDR)
         bme280_i2c.set_default_bus(I2C_BUS)
 
-    def setup(self):
+    def setup(self) -> None:
         bme280.setup()
 
-    def measure(self):
+    def measure(self) -> None:
         with lock:
             self.data = bme280.read_all()
 
-    def get_dict(self):
+    def get_dict(self) -> dict[str, Any]:
         return {
             "temperature": self.data.temperature,
             "humidity": self.data.humidity,
@@ -107,7 +114,7 @@ def calc_tdi(temperature: float, humidity: float) -> float:
     return 0.81 * temperature + 0.01 * humidity * (0.99 * temperature - 14.3) + 46.3
 
 
-def measurement_thread(e, data):
+def measurement_thread(e: threading.Event, data: dict[str, Any]) -> None:
     sensor = Bme280Sensor()
     sensor.setup()
 
@@ -122,7 +129,7 @@ def measurement_thread(e, data):
         time.sleep(1)
 
 
-def display_thread(e, data):
+def display_thread(e: threading.Event, data: dict[str, Any]) -> None:
     display = SO1602ADisplay()
     display.setup()
 
@@ -147,7 +154,7 @@ def display_thread(e, data):
         time.sleep(2)
 
 
-def send_data_thread(e, data):
+def send_data_thread(e: threading.Event, data: dict[str, Any]) -> None:
     writer = InfluxWriter()
 
     while not e.is_set():
@@ -159,9 +166,9 @@ def send_data_thread(e, data):
         time.sleep(10)
 
 
-def control_thread():
+def control_thread() -> None:
     e = threading.Event()
-    data = {}
+    data: dict[str, Any] = {}
     threads = [
         threading.Thread(target=measurement_thread, args=(e, data)),
         threading.Thread(target=display_thread, args=(e, data)),
@@ -181,7 +188,7 @@ def control_thread():
         t.join()
 
 
-def main():
+def main() -> None:
     control_thread()
 
 
